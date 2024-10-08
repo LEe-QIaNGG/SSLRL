36eeefa659fbee234043aac7724ed762cdd1ca29
LWY <1259315821@qq.com>
2024-10-08 14:54:41 +0800

    1

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-import torch
+import gymnasium as gym
 
-print(torch.cuda.is_available())
+# 加载某个 Atari 游戏环境
+env = gym.make('CartPole-v1')
+
+# 查看环境的 reward_threshold 属性
+print(env.spec.reward_threshold)
